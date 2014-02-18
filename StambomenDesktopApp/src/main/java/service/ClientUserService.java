ecefd242cad5b5369153f9d4d71faa0f3c9e6dd8
Axl Francois <axlfrancois@gmail.com>
2014-02-18 17:21:09 +0100

    Changed some UI things. Starting to make this client work! :)

--- StambomenDesktopApp/src/main/java/service/ClientUserService.java
+++ StambomenDesktopApp/src/main/java/service/ClientUserService.java
@@ -6,6 +6,7 @@ import com.sun.jersey.api.client.WebResource;
 
 public class ClientUserService 
 {
+    /*Not finished yet, simply a test. USERDTO Should be found in the project.*/
     public void makeUser(UserDTO user)
     {
 		Client client = Client.create();
