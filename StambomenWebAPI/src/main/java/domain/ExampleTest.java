ff28816b3fae115fda6efef70e62bbf1e8ef1878
Jelle Verreth <jelle.verreth@gmail.com>
2014-02-26 22:24:51 +0100

    mock tests

---
+++ StambomenWebAPI/src/main/java/domain/ExampleTest.java
@@ -0,0 +1,20 @@
+package domain;
+import org.junit.*;
+
+public class ExampleTest {
+    private ClassUnderTest classUnderTest;
+    private Collaborator mock;
+
+    @Before
+    public void setUp() {
+        classUnderTest = new ClassUnderTest();
+        classUnderTest.setListener(mock);
+    }
+
+    @Test
+    public void testRemoveNonExistingDocument() {    
+        // This call should not lead to any notification
+        // of the Mock Object: 
+        classUnderTest.removeDocument("Does not exist");
+    }
+}
