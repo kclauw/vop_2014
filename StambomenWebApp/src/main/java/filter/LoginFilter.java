0d0aead03a7a873804f89606f574a14994002d0c
lrhuyghe <lowie.huyghe@ugent.be>
2014-03-03 17:33:29 +0100

    Werkt nu met echte gebruikers en echt bomen die in db zitten.

--- StambomenWebApp/src/main/java/filter/LoginFilter.java
+++ StambomenWebApp/src/main/java/filter/LoginFilter.java
@@ -15,7 +15,6 @@ import dto.UserDTO;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.List;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -26,7 +25,10 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import service.ClientServiceController;
 import service.ClientTreeController;
+import service.ClientUserController;
+import service.ClientUserService;
 
 /**
  *
@@ -69,12 +71,13 @@ public class LoginFilter implements Filter {
                 session = null;
             }
             
-            if (username.equals("admin") && password.equals("admin"))
+            ClientUserController usercontroller = new ClientUserController();
+            
+            UserDTO user = new UserDTO(0, username, password);
+            String loginResponse = usercontroller.login(user);
+            if (loginResponse == null)
             {
-                UserDTO user = new UserDTO(8, username, password);
-                
-//                ClientUserService userservice = new ClientUserService();
-//                userservice.login(user);
+                user = ClientServiceController.getInstance().getUser();
                 
                 session = request.getSession();
                 session.setAttribute("user", user);
@@ -82,7 +85,7 @@ public class LoginFilter implements Filter {
 
                 response.sendRedirect(contextpath + "/main.jsp");
             } else {
-                req.setAttribute("errormessage", "Verkeerde gebruikersnaam of paswoord.");
+                req.setAttribute("errormessage", loginResponse);
             }
         }
         
@@ -118,45 +121,10 @@ public class LoginFilter implements Filter {
     }
     
     private void initUserData(HttpSession session, UserDTO user) {
-//        ClientTreeController controller = new ClientTreeController();
-//        List<TreeDTO> trees = controller.getTrees(user.getId());
-        List<TreeDTO> trees = new ArrayList<TreeDTO>();
-        TreeDTO tree = new TreeDTO(0, user, PrivacyDTO.PUBLIC, "Family Huyghe", null);
-        trees.add(tree);
-        trees.add(new TreeDTO(1, user, PrivacyDTO.FRIENDS, "Family Demey", null));
-        trees.add(new TreeDTO(2, user, PrivacyDTO.PRIVATE, "Family Hooghe", null));
-        trees.add(new TreeDTO(3, user, PrivacyDTO.PRIVATE, "Family Vanpeperstraete", null));
         
-        PersonDTO father = new PersonDTO(0, "Mario", "Huyghe", GenderDTO.MALE, null, null, null, null, null);
-        PersonDTO mother = new PersonDTO(1, "Jo", "Demey", GenderDTO.FEMALE, null, null, null, null, null);
-        tree.setPersons(new ArrayList<PersonDTO>());
-        tree.getPersons().add(father);
-        tree.getPersons().add(mother);
+        ClientTreeController controller = new ClientTreeController();
         
-        Calendar birthdate = Calendar.getInstance();
-        birthdate.set(1992, 5, 9);
-        PersonDTO childfather = new PersonDTO(2, "Lowie", "Huyghe", GenderDTO.MALE, birthdate.getTime(), null, new PlaceDTO(0, 0, 0, null, "België", "8500", "Kortrijk"), father, mother);
-        PersonDTO childmother = new PersonDTO(3, "Emma", "Watson", GenderDTO.FEMALE, null, null, null, null, null);
-        tree.getPersons().add(childfather);
-        tree.getPersons().add(childmother);
-        tree.getPersons().add(new PersonDTO(4, "Pedro", "Huyghe", GenderDTO.MALE, null, null, null, childfather, childmother));
-        tree.getPersons().add(new PersonDTO(5, "Erna", "Huyghe", GenderDTO.FEMALE, null, null, null, childfather, childmother));
-        
-        PersonDTO grandchildfather = new PersonDTO(6, "Jef", "Huyghe", GenderDTO.MALE, null, null, null, childfather, childmother);
-        PersonDTO grandchildmother = new PersonDTO(7, "Mila", "Kunis", GenderDTO.FEMALE, null, null, null, null, null);
-        tree.getPersons().add(grandchildfather);
-        tree.getPersons().add(grandchildmother);
-        tree.getPersons().add(new PersonDTO(8, "Wolf", "Huyghe", GenderDTO.MALE, null, null, null, grandchildfather, grandchildmother));
-        tree.getPersons().add(new PersonDTO(9, "Winter", "Huyghe", GenderDTO.MALE, null, null, null, grandchildfather, grandchildmother));
-        tree.getPersons().add(new PersonDTO(10, "Lisa", "Huyghe", GenderDTO.FEMALE, null, null, null, grandchildfather, grandchildmother));
-        tree.getPersons().add(new PersonDTO(11, "Lisa", "Huyghe", GenderDTO.FEMALE, null, null, null, grandchildfather, grandchildmother));
-        tree.getPersons().add(new PersonDTO(12, "Lisa", "Huyghe", GenderDTO.FEMALE, null, null, null, grandchildfather, grandchildmother));
-        
-        tree.getPersons().add(new PersonDTO(13, "Marie", "Huyghe", GenderDTO.FEMALE, null, null, null, father, mother));
-        tree.getPersons().add(new PersonDTO(14, "Emiel", "Huyghe", GenderDTO.MALE, null, null, null, father, mother));
-        
-        session.setAttribute("trees", trees);
-        
+        session.setAttribute("trees", controller.getTrees(user.getId()));
         
         List<UserDTO> friends = new ArrayList<UserDTO>();
         friends.add(new UserDTO(0, "blowfish", "zdqzdqzd"));
