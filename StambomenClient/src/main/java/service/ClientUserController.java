ee7dd2d41b1b0acfb85d4e9726f7f5a9852c0685
Sander <sander.trypsteen@ugent.be>
2014-03-03 20:29:24 +0100

    getfriends implementation in StambomenClient

--- StambomenClient/src/main/java/service/ClientUserController.java
+++ StambomenClient/src/main/java/service/ClientUserController.java
@@ -1,26 +1,28 @@
 package service;
 
 import dto.UserDTO;
+import java.util.Map;
 
-public class ClientUserController
-{
+public class ClientUserController {
 
     private ClientUserService client;
 
-    public ClientUserController()
-    {
+    public ClientUserController() {
         this.client = new ClientUserService();
     }
 
-    public String makeUser(UserDTO user)
-    {
+    public String makeUser(UserDTO user) {
         return client.makeUser(user);
     }
 
-    public String login(UserDTO user)
-    {
+    public String login(UserDTO user) {
         System.out.println("[CLIENT USER CONTROLLER] LOGIN CLIENT USER SERVICE");
         return client.login(user);
     }
 
+    public Map<String, Integer> getFriends() {
+        int userID = ClientServiceController.getInstance().getUser().getId();
+
+        return client.getFriends(userID);
+    }
 }
