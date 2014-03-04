a29bcccd35abd0b4a2459c5258cd2d604e63e341
Jelle Verreth <jelle.verreth@gmail.com>
2014-03-04 12:20:01 +0100

    addtreepanel

--- StambomenWebAPI/src/main/java/service/TreeService.java
+++ StambomenWebAPI/src/main/java/service/TreeService.java
@@ -2,7 +2,9 @@ package service;
 
 import domain.Tree;
 import domain.controller.TreeController;
+import exception.TreeAlreadyExistsException;
 import java.util.List;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -10,6 +12,8 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.POST;
 
 @Path("/tree")
 public class TreeService
@@ -38,6 +42,24 @@ public class TreeService
         logger.info("[TREE SERVICE][GET] Getting trees by userid" + userId);
         List<Tree> tr = tc.getTrees(userId);
         return tr;
+    }
+    
+    @POST
+    @Path("/post")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response makeTree(Tree tree)
+    {
+        try
+        {
+            String result = "Tree added:" + tree.toString();
+            tc.addTree(tree);
+            return Response.status(Response.Status.OK).entity(result).build();
+        }
+        catch (TreeAlreadyExistsException ex)
+        {
+            return Response.status(Response.Status.NOT_ACCEPTABLE).entity(ex.getMessage()).build();
+        }
+
     }
 
 }
