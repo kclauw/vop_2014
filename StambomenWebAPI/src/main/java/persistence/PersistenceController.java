8432318edf3eaac168fc9b02c6f7e64a8b24d06e
Sander <sander.trypsteen@ugent.be>
2014-03-04 17:22:52 +0100

    added on stambomenwebapi user sendFriendrequest user acceptFriendrequest user deniesFriendrequest user deletesFriend

--- StambomenWebAPI/src/main/java/persistence/PersistenceController.java
+++ StambomenWebAPI/src/main/java/persistence/PersistenceController.java
@@ -9,8 +9,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class PersistenceController
-{
+public class PersistenceController {
 
     private UserDao userDao;
     private TreeDao treeDao;
@@ -18,8 +17,7 @@ public class PersistenceController
     private PlaceDao placeDao;
     private final Logger logger;
 
-    public PersistenceController()
-    {
+    public PersistenceController() {
         userDao = new UserDao();
         treeDao = new TreeDao(this);
         personDao = new PersonDao(this);
@@ -28,99 +26,102 @@ public class PersistenceController
 
     }
 
-    public void addUser(User user)
-    {
+    public void addUser(User user) {
         logger.info("[PERSISTENCE CONTROLLER] Add User " + user);
         userDao.save(user);
     }
 
-    public List<User> getUsers()
-    {
+    public List<User> getUsers() {
         logger.info("[PERSISTENCE CONTROLLER] Get all users");
         return (List<User>) userDao.getAll();
     }
 
-    public User getUser(String username)
-    {
+    public User getUser(String username) {
         logger.info("[PERSISTENCE CONTROLLER] Get user with username" + username);
         return userDao.get(username);
     }
 
-    public User getUser(int id)
-    {
+    public User getUser(int id) {
         logger.info("[PERSISTENCE CONTROLLER] Get user with id" + id);
         return userDao.get(id);
     }
 
-    public Tree getTree(int id)
-    {
+    public Tree getTree(int id) {
         logger.info("[PERSISTENCE CONTROLLER] Get tree with id" + id);
         return treeDao.get(id);
     }
 
-    public void addTree(Tree tree)
-    {
+    public void addTree(Tree tree) {
         logger.info("[PERSISTENCE CONTROLLER] Add tree : " + tree);
         treeDao.save(tree);
     }
 
-    public Place getPlace(int placeId)
-    {
+    public Place getPlace(int placeId) {
         logger.info("[PERSISTENCE CONTROLLER] Get place with id " + placeId);
         return placeDao.get(placeId);
     }
 
-    public List<Person> getPersons(int treeId)
-    {
+    public List<Person> getPersons(int treeId) {
         logger.info("[PERSISTENCE CONTROLLER] Get persons from tree with id " + treeId);
         return (List<Person>) personDao.GetAll(treeId);
     }
 
-    public List<Tree> getTrees(int userId)
-    {
+    public List<Tree> getTrees(int userId) {
         logger.info("[PERSISTENCE CONTROLLER] Get trees from user " + userId);
         return treeDao.getAll(userId);
     }
 
-    public List<User> getFriends(int userID)
-    {
+    public List<User> getFriends(int userID) {
         logger.info("[PERSISTENCE CONTROLLER] Get friends from user " + userID);
         return userDao.getFriends(userID);
     }
 
-    public Place getPlace(ResultSet res)
-    {
+    public Place getPlace(ResultSet res) {
         logger.info("[PERSISTENCE CONTROLLER] Get place based on a resultset");
         return placeDao.map(res);
     }
 
-    public void addPerson(Person person)
-    {
+    public void addPerson(Person person) {
         logger.info("[PERSISTENCE CONTROLLER] Add person " + person);
         personDao.save(person);
     }
 
-    public void removePerson(Person person)
-    {
+    public void removePerson(Person person) {
         logger.info("[PERSISTENCE CONTROLLER] Remove person " + person);
         personDao.delete(person);
     }
 
-    public Person getPerson(int personId)
-    {
+    public Person getPerson(int personId) {
         logger.info("[PERSISTENCE CONTROLLER] Get person with id" + personId);
         return personDao.get(personId);
     }
 
-    public void updatePerson(Person person)
-    {
+    public void updatePerson(Person person) {
         logger.info("[PERSISTENCE CONTROLLER] Update person " + person);
         personDao.update(person);
     }
 
-    public List<User> getFriendRequest(int userID)
-    {
+    public List<User> getFriendRequest(int userID) {
         return userDao.getFriendRequest(userID);
     }
 
+    public void sendFriendRequest(int friendId, int userId) {
+        logger.info("[PERSISTENCE CONTROLLER] Send friend request from: " + userId + " to: " + friendId);
+        userDao.sendFriendRequest(friendId, userId);
+    }
+
+    public void acceptFriendRequest(int friendId, int userId) {
+        logger.info("[PERSISTENCE CONTROLLER] accept friend request from: " + userId + " to: " + friendId);
+        userDao.acceptFriendRequest(friendId, userId);
+    }
+
+    public void denyFriendRequest(int friendId, int userId) {
+        logger.info("[PERSISTENCE CONTROLLER] Deny friend request from: " + userId + " to: " + friendId);
+        userDao.denyFriendRequest(friendId, userId);
+    }
+
+    public void deleteFriend(int friendId, int userId) {
+        logger.info("[PERSISTENCE CONTROLLER] delete friend from: " + userId + " to: " + friendId);
+        userDao.deleteFriend(friendId, userId);
+    }
 }
