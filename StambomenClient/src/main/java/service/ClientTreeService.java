25466c2e867165d8d2ef24b607f697d8d588089f
Axl Francois <axlfrancois@gmail.com>
2014-03-04 12:30:26 +0100

    Adding tree panel

--- StambomenClient/src/main/java/service/ClientTreeService.java
+++ StambomenClient/src/main/java/service/ClientTreeService.java
@@ -17,18 +17,6 @@ public class ClientTreeService
 
     public String makeTree(TreeDTO treeDTO)
     {
-//        ClientConfig clientConfig = new DefaultClientConfig();
-//        clientConfig.getFeatures().put(JSONConfiguration.FEATURE_POJO_MAPPING, Boolean.TRUE);
-//        Client client = Client.create(clientConfig);
-//        WebResource webResource = client.resource(url + "/post");
-//        String json = new Gson().toJson(treeDTO);
-//        ClientResponse response = webResource.accept("application/json")
-//                .type("application/json").post(ClientResponse.class, json);
-//
-//        if (response.getStatus() != 200)
-//        {
-//            return response.getEntity(String.class);
-//        }
 
         return null;
     }
