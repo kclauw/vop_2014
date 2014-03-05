fbc7e01597cb41f0ff2ac90bca5f8577a50e9163
Axl Francois <axlfrancois@gmail.com>
2014-03-05 12:24:03 +0100

    Update of person

--- StambomenClient/src/main/java/service/ClientPersonController.java
+++ StambomenClient/src/main/java/service/ClientPersonController.java
@@ -2,21 +2,32 @@ package service;
 
 import dto.PersonDTO;
 
-public class ClientPersonController {
+public class ClientPersonController
+{
 
     private ClientPersonService clientPersonService;
 
-    public ClientPersonController(ClientPersonService clientPersonService) {
+    public ClientPersonController(ClientPersonService clientPersonService)
+    {
         System.out.println("[CLIENT PERSON CONTROLLER]");
         this.clientPersonService = clientPersonService;
     }
 
-    public String savePerson(PersonDTO person) {
+    public String savePerson(PersonDTO person)
+    {
         return clientPersonService.savePerson(person);
     }
 
-    public String deletePerson(PersonDTO person) {
+    public String deletePerson(PersonDTO person)
+    {
         System.out.println("[CLIENT PERSON CONTROLLER] DELETING PERSON " + person.toString());
         return clientPersonService.deletePerson(person);
     }
+
+    public String updatePerson(PersonDTO person)
+    {
+        System.out.println("[CLIENT PERSON CONTROLLER] Updating PERSON " + person.toString());
+        return clientPersonService.updatePerson(person);
+    }
+
 }
