e27b5bf3c7439f246de727dd70c80316f67d2309
Axl Francois <axlfrancois@gmail.com>
2014-03-07 21:48:16 +0100

    Fix a small problem.

--- StambomenWebAPI/src/main/java/persistence/DatabaseUtils.java
+++ StambomenWebAPI/src/main/java/persistence/DatabaseUtils.java
@@ -8,60 +8,75 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public class DatabaseUtils {
+public class DatabaseUtils
+{
 
     protected final static String driver = "com.mysql.jdbc.Driver";
     protected final static String username = "team12";
     protected final static String password = "RKAxujnJ";
     protected static String url;
 
-    private DatabaseUtils() throws Exception {
-        setUrlPath();
+    private DatabaseUtils() throws Exception
+    {
     }
 
-    public static Connection getCon(String driver, String url, String username, String password) throws Exception {
+    public static Connection getCon(String driver, String url, String username, String password) throws Exception
+    {
         Class.forName(driver);
+        setUrlPath();
         Connection con = DriverManager.getConnection(url, username, password);
-
         return con;
     }
 
-    public static Connection getConnection() throws Exception {
+    public static Connection getConnection() throws Exception
+    {
         Connection con = getCon(driver, url, username, password);
 
         return con;
     }
 
-    public static void closeQuietly(Connection conn) throws SQLException {
-        if (conn != null) {
+    public static void closeQuietly(Connection conn) throws SQLException
+    {
+        if (conn != null)
+        {
             conn.close();
         }
     }
 
-    public static void closeQuietly(ResultSet rs) throws SQLException {
-        if (rs != null) {
+    public static void closeQuietly(ResultSet rs) throws SQLException
+    {
+        if (rs != null)
+        {
             rs.close();
         }
     }
 
-    public static void closeQuietly(PreparedStatement ps) throws SQLException {
-        if (ps != null) {
+    public static void closeQuietly(PreparedStatement ps) throws SQLException
+    {
+        if (ps != null)
+        {
             ps.close();
         }
     }
 
-    private static void setUrlPath() throws UnknownHostException {
+    private static void setUrlPath() throws UnknownHostException
+    {
         InetAddress ip;
         String hostname;
         ip = InetAddress.getLocalHost();
         hostname = ip.getHostName();
         String urlPrefix = "jdbc:mysql://db.vop.tiwi.be";
 
-        if (hostname.equals("staging")) {
+        if (hostname.equals("staging"))
+        {
             url = urlPrefix + ":3306/team12_staging?";
-        } else if (hostname.equals("release")) {
+        }
+        else if (hostname.equals("release"))
+        {
             url = urlPrefix + ":3306/team12_release?";
-        } else {
+        }
+        else
+        {
             url = urlPrefix + ":443/team12_staging?";
         }
     }
