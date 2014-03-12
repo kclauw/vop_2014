f0f02bc512ab8910c617b9e2a1ab9e9c78ff24d1
Sklipnoty <axlfrancois@gmail.com>
2014-03-12 23:41:49 +0100

    Fixing the language problem. Fixing wrong use of builder.

--- StambomenClient/src/main/java/StartUp.java
+++ StambomenClient/src/main/java/StartUp.java
@@ -12,19 +12,19 @@ public class StartUp
 
     public static void main(String args[])
     {
-        ClientUserController uc = new ClientUserController();
-        UserDTO user = new UserDTO(-1, "Jelle", "123", null);
-        String login = uc.login(user);
-
-        ClientTreeController ctc = new ClientTreeController();
-        List<TreeDTO> tres = ctc.getTrees(0);
-
-        ClientPersonController cpc = new ClientPersonController();
-
-        PersonDTO pe = tres.get(0).getPersons().get(0);
-        pe.setFirstName("TEMPLE");
-        cpc.updatePerson(pe);
-
-        System.out.println("LOGIN " + login);
+//        ClientUserController uc = new ClientUserController();
+//        UserDTO user = new UserDTO(-1, "Jelle", "123", null);
+//        String login = uc.login(user);
+//
+//        ClientTreeController ctc = new ClientTreeController();
+//        List<TreeDTO> tres = ctc.getTrees(0);
+//
+//        ClientPersonController cpc = new ClientPersonController();
+//
+//        PersonDTO pe = tres.get(0).getPersons().get(0);
+//        pe.setFirstName("TEMPLE");
+//        cpc.updatePerson(pe);
+//
+//        System.out.println("LOGIN " + login);
     }
 }
