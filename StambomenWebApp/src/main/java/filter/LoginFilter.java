984ec14d5f8715e41a8e64e9025e8ab7809f63e8
Sklipnoty <axlfrancois@gmail.com>
2014-03-12 23:23:49 +0100

    Fixing the language problem. Fixing wrong use of builder.

--- StambomenWebApp/src/main/java/filter/LoginFilter.java
+++ StambomenWebApp/src/main/java/filter/LoginFilter.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package filter;
 
 import dto.UserDTO;
@@ -25,112 +24,134 @@ import service.ClientTreeController;
 import service.ClientUserController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author Lowie
  */
-public class LoginFilter implements Filter {
+public class LoginFilter implements Filter
+{
 
     private final Logger logger = LoggerFactory.getLogger(getClass());
+
     @Override
-    public void init(FilterConfig config) throws ServletException {
+    public void init(FilterConfig config) throws ServletException
+    {
         // If you have any <init-param> in web.xml, then you could get them
         // here by config.getInitParameter("name") and assign it as field.
     }
 
     @Override
-    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException
+    {
         logger.info("[LOGIN FILTER][DO FILTER]:" + req.toString() + " " + res.toString() + " " + chain.toString());
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
         HttpSession session = request.getSession(false);
-        
+
         String requesturi = request.getRequestURI();
         String contextpath = request.getContextPath();
-        
+
         String username = request.getParameter("username");
         String password = request.getParameter("password");
         String logout = request.getParameter("logout");
-        
+
         if (logout != null)
         {
             if (session != null)
             {
                 if (logout != null && session != null)
+                {
                     logger.info("[LOGIN FILTER][DO FILTER]" + logout.toString() + " " + session.toString());
+                }
                 session.invalidate();
                 session = null;
             }
         }
-        else if (username != null  && password != null)
+        else if (username != null && password != null)
         {
             if (session != null)
             {
                 session.invalidate();
                 session = null;
             }
-            
+
             ClientUserController userController = new ClientUserController();
-            
-            UserDTO user = new UserDTO(0, username, password);
+
+            UserDTO user = new UserDTO(-1, username, password, null);
             if (username != null)
+            {
                 logger.info("[LOGIN FILTER][DO FILTER] NEW USER:" + username.toString());
+            }
             String loginResponse = userController.login(user);
             if (loginResponse == null)
             {
                 session = request.getSession();
-                
+
                 ClientServiceController serviceController = ClientServiceController.getInstance();
                 ClientTreeController treeController = new ClientTreeController();
                 session.setAttribute("serviceController", serviceController);
                 session.setAttribute("treeController", treeController);
                 session.setAttribute("userController", userController);
-                
+
                 user = serviceController.getUser();
-                
+
                 session.setAttribute("user", user);
                 initUserData(session, user);
 
                 response.sendRedirect(contextpath + "/main.jsp");
-            } else {
+            }
+            else
+            {
                 req.setAttribute("errormessage", loginResponse);
             }
         }
-        
+
         List<String> allowedExtensions = new ArrayList<String>();
         allowedExtensions.add("css");
         allowedExtensions.add("js");
         allowedExtensions.add("png");
         allowedExtensions.add("jpg");
-        
+
         boolean allowed = false;
-        for (String ext : allowedExtensions) {
-            if (requesturi.endsWith("." + ext)){
+        for (String ext : allowedExtensions)
+        {
+            if (requesturi.endsWith("." + ext))
+            {
                 allowed = true;
                 break;
             }
         }
-        if (allowed || (session != null && session.getAttribute("user") != null) || requesturi.endsWith(contextpath + "/login.jsp")) {
+        if (allowed || (session != null && session.getAttribute("user") != null) || requesturi.endsWith(contextpath + "/login.jsp"))
+        {
             if (requesturi.endsWith(contextpath + "/index.jsp"))
             {
                 response.sendRedirect(contextpath + "/main.jsp");
-            } else {
+            }
+            else
+            {
                 chain.doFilter(req, res);
             }
-        } else {
+        }
+        else
+        {
             response.sendRedirect(contextpath + "/login.jsp");
         }
     }
 
     @Override
-    public void destroy() {
+    public void destroy()
+    {
         // If you have assigned any expensive resources as field of
         // this Filter class, then you could clean/close them here.
     }
-    
-    private void initUserData(HttpSession session, UserDTO user) {
+
+    private void initUserData(HttpSession session, UserDTO user)
+    {
         if (session != null && user != null)
+        {
             logger.info("[LOGIN FILTER][INIT USER DATA]HTTP SESSION:" + session.toString() + "USERDTO " + user.toString());
+        }
         ClientTreeController treeController = (ClientTreeController) session.getAttribute("treeController");
         session.setAttribute("trees", treeController.getTrees(user.getId()));
     }
