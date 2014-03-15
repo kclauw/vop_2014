acc93032bdc3c46683f51b7504c189ad4edad95d
Sander <sander.trypsteen@ugent.be>
2014-03-15 17:50:03 +0100

    added setprofilePrivacy getpublicprofile getpublicprofiles in stambomenWebClient

--- StambomenClient/src/main/java/service/ClientUserService.java
+++ StambomenClient/src/main/java/service/ClientUserService.java
@@ -17,21 +17,18 @@ import org.slf4j.LoggerFactory;
 /**
  * Contains the concrete implementations for the request to the WebAPI.
  */
-public class ClientUserService
-{
+public class ClientUserService {
 
     private final static String url = ServiceConstant.getInstance().getURL();
     private final Logger logger = LoggerFactory.getLogger(getClass());
 
-    public String makeUser(UserDTO userDTO)
-    {
+    public String makeUser(UserDTO userDTO) {
         logger.info("[CLIENT USER SERVICE][MAKE USER]Make user:" + userDTO.toString());
         Client client = ClientServiceController.getInstance().getClient();
 
         String json = new Gson().toJson(userDTO);
         Response response = client.target(url + "user/post").request(MediaType.APPLICATION_JSON).post(Entity.entity(json, MediaType.APPLICATION_JSON));
-        if (response.getStatus() != 200)
-        {
+        if (response.getStatus() != 200) {
 
             return " " + response.readEntity(String.class);
         }
@@ -39,8 +36,7 @@ public class ClientUserService
         return null;
     }
 
-    public String login(UserDTO user)
-    {
+    public String login(UserDTO user) {
         logger.info("[CLIENT USER SERVICE][LOGIN]Login of user:" + user.toString());
         Client client = ClientBuilder.newClient();
         client.register(new JacksonFeature());
@@ -49,8 +45,7 @@ public class ClientUserService
 
         UserDTO dto = client.target(url + "user/login/" + user.getUsername()).request("application/json").accept("application/json").get(UserDTO.class);
 
-        if (dto == null)
-        {
+        if (dto == null) {
             System.out.println("USER NOT FOUND");
             return "Error";
         }
@@ -60,39 +55,33 @@ public class ClientUserService
         return null;
     }
 
-    public List<UserDTO> getFriends(int userID)
-    {
+    public List<UserDTO> getFriends(int userID) {
         logger.info("[CLIENT USER SERVICE][GET FRIENDS]Getting friends for user: " + userID);
 
         Client client = ClientServiceController.getInstance().getClient();
         client.register(new JacksonFeature());
-        List<UserDTO> friends = client.target(url + "user/friends/" + userID).request(MediaType.APPLICATION_JSON).get(new GenericType<List<UserDTO>>()
-        {
+        List<UserDTO> friends = client.target(url + "user/friends/" + userID).request(MediaType.APPLICATION_JSON).get(new GenericType<List<UserDTO>>() {
         });
 
         return friends;
     }
 
-    public List<UserDTO> getFriendRequests(int userID)
-    {
+    public List<UserDTO> getFriendRequests(int userID) {
         logger.info("[CLIENT USER SERVICE][GET FRIEND REQUESTS]Getting friendsfor user with id:" + userID);
         Client client = ClientServiceController.getInstance().getClient();
         client.register(new JacksonFeature());
-        List<UserDTO> friends = client.target(url + "user/friends/requests/" + userID).request(MediaType.APPLICATION_JSON).get(new GenericType<List<UserDTO>>()
-        {
+        List<UserDTO> friends = client.target(url + "user/friends/requests/" + userID).request(MediaType.APPLICATION_JSON).get(new GenericType<List<UserDTO>>() {
         });
 
         return friends;
     }
 
-    public String deleteFriend(int userID, int frienduserID)
-    {
+    public String deleteFriend(int userID, int frienduserID) {
         logger.info("[CLIENT USER SERVICE][DELETE FRIEND]Delete friend with id:" + frienduserID + " for user with id: " + userID);
         Client client = ClientServiceController.getInstance().getClient();
         client.register(new JacksonFeature());
         Response response = client.target(url + "user/friends/delete/" + userID + "/" + frienduserID).request(MediaType.APPLICATION_JSON).get();
-        if (response.getStatus() != 200)
-        {
+        if (response.getStatus() != 200) {
 
             return " " + response.readEntity(String.class);
         }
@@ -100,14 +89,12 @@ public class ClientUserService
         return null;
     }
 
-    public String allowDenyFriendRequest(int userID, int frienduserID, boolean allow)
-    {
+    public String allowDenyFriendRequest(int userID, int frienduserID, boolean allow) {
         logger.info("[CLIENT USER SERVICE][ALLOW DENY FRIEND REQUEST]Allow deny friendrequest for friend with id:" + frienduserID + " for user with id: " + userID);
         Client client = ClientServiceController.getInstance().getClient();
         client.register(new JacksonFeature());
         Response response = client.target(url + "user/friends/requests/" + (allow ? "allow" : "deny") + "/" + userID + "/" + frienduserID).request(MediaType.APPLICATION_JSON).get();
-        if (response.getStatus() != 200)
-        {
+        if (response.getStatus() != 200) {
 
             return " " + response.readEntity(String.class);
         }
@@ -115,14 +102,12 @@ public class ClientUserService
         return null;
     }
 
-    public String sendFriendRequest(int userID, String frienduserName)
-    {
+    public String sendFriendRequest(int userID, String frienduserName) {
         logger.info("[CLIENT USER SERVICE][SEND FRIEND REQUEST]Send friendrequest to user with id:" + userID + " to friend user with name" + frienduserName);
         Client client = ClientServiceController.getInstance().getClient();
         client.register(new JacksonFeature());
         Response response = client.target(url + "user/friends/requests/send/" + userID + "/" + frienduserName).request(MediaType.APPLICATION_JSON).get();
-        if (response.getStatus() != 200)
-        {
+        if (response.getStatus() != 200) {
 
             return " " + response.readEntity(String.class);
         }
@@ -130,9 +115,43 @@ public class ClientUserService
         return null;
     }
 
-    public void setLanguage(int userID, int language)
-    {
+    public void setLanguage(int userID, int language) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
+    public String setUserPrivacy(int userID, int userPrivacy) {
+        logger.info("[CLIENT USER SERVICE][SET USER PRIVACY]Set user privacy for user with id:" + userID + "to privacy state:" + userPrivacy);
+        Client client = ClientServiceController.getInstance().getClient();
+        client.register(new JacksonFeature());
+
+        Response response = client.target(url + "user/profile/setUserPrivacy/" + userID + "/" + userPrivacy).request(MediaType.APPLICATION_JSON).get();
+        if (response.getStatus() != 200) {
+
+            return " " + response.readEntity(String.class);
+        }
+
+        return null;
+    }
+
+    public UserDTO getPublicUserProfile(int userProfileID) {
+        logger.info("[CLIENT USER SERVICE][GET PUBLIC USER PROFILES]Get a public user profile");
+        Client client = ClientServiceController.getInstance().getClient();
+        client.register(new JacksonFeature());
+
+        UserDTO userProfile = client.target(url + "user/profile/getPublicUserProfile/" + userProfileID).request(MediaType.APPLICATION_JSON).get(new GenericType<UserDTO>() {
+        });
+
+        return userProfile;
+    }
+
+    public List<UserDTO> getPublicUserProfiles(int userID) {
+        logger.info("[CLIENT USER SERVICE][GET PUBLIC USER PROFILES]Get all public user profiles");
+        Client client = ClientServiceController.getInstance().getClient();
+        client.register(new JacksonFeature());
+
+        List<UserDTO> userProfiles = client.target(url + "user/profile/getPublicUserProfile/" + userID).request(MediaType.APPLICATION_JSON).get(new GenericType<List<UserDTO>>() {
+        });
+
+        return userProfiles;
+    }
 }
