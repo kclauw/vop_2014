e9ab9e6933584c5401c0b2925724e6a5cfac5d66
Jelle Verreth <jelle.verreth@gmail.com>
2014-03-17 10:31:15 +0100

    Merge branch 'master' of https://github.ugent.be/iii-vop/vop-2014-team12
    Conflicts:
    	StambomenClient/src/main/java/dto/PersonDTO.java

---
+++ StambomenClient/src/main/java/util/Translator.java
@@ -0,0 +1,31 @@
+package util;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+public class Translator
+{
+
+    private ResourceBundle messages;
+
+    public Translator()
+    {
+        selectLanguage("en");
+    }
+
+    public void selectLanguage(String language)
+    {
+        messages = ResourceBundle.getBundle("MessagesBundle",
+                new Locale(language));
+    }
+
+    public String translate(String message)
+    {
+        return messages.getString(message);
+    }
+
+    public String giveCurrentLanguage()
+    {
+        return messages.getLocale().getLanguage();
+    }
+}
