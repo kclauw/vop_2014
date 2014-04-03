1ab7537aa71595efa050ff5979601e0f5d7993be
Jelle Verreth <jelle.verreth@gmail.com>
2014-04-03 11:41:37 +0200

    logging

---
+++ StambomenWebAPI/src/main/java/domain/Event.java
@@ -0,0 +1,31 @@
+package domain;
+
+public enum Event
+{
+
+    ADDTREE(0), DELTREE(1), CHATREE(2), ADDPER(3), DELPER(4), CHAPER(5), ADDFRIEND(6);
+
+    private int eventId;
+
+    Event(int eventId)
+    {
+        this.eventId = eventId;
+    }
+
+    public int getEventId()
+    {
+        return this.eventId;
+    }
+
+    public static Event getEventId(int eventId)
+    {
+        for (Event v : Event.values())
+        {
+            if (v.getEventId() == eventId)
+            {
+                return v;
+            }
+        }
+        return null;
+    }
+}
