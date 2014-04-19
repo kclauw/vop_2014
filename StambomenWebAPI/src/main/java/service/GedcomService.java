9f97a08e9752ae3d4827214b33dd5118e9a0a94d
Axl Francois <axlfrancois@gmail.com>
2014-04-19 14:36:40 +0200

    Kenzo fix

--- StambomenWebAPI/src/main/java/service/GedcomService.java
+++ StambomenWebAPI/src/main/java/service/GedcomService.java
@@ -1,29 +1,20 @@
 package service;
 
-import domain.Person;
 import domain.Tree;
 import domain.User;
 import domain.controller.GedcomController;
-import domain.controller.PersonController;
-import domain.enums.PersonAdd;
-import exception.PersonAlreadyExistsException;
-import java.io.BufferedInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.List;
-import javax.imageio.ImageIO;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import org.gedcom4j.parser.GedcomParserException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.commons.io.IOUtils;
-import org.gedcom4j.parser.GedcomParserException;
 
 @Path("/gedcom")
 public class GedcomService
@@ -37,13 +28,23 @@ public class GedcomService
     @Path("/import/{user}/{tree}")
     @Consumes(MediaType.APPLICATION_OCTET_STREAM)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response importGedcom(@PathParam("user") User user,@PathParam("tree") Tree tree, InputStream inp) throws IOException, GedcomParserException
+    public Response importGedcom(@PathParam("user") User user, @PathParam("tree") Tree tree, InputStream inp)
     {
-
-        String result = "Importing new Gedcom for user " + user + " for tree : " + tree;
-        System.out.println(result);
-        gc.importGedcom(user,tree, inp);
-        return Response.status(Response.Status.OK).entity(result).build();
+        try
+        {
+            String result = "Importing new Gedcom for user " + user + " for tree : " + tree;
+            System.out.println(result);
+            gc.importGedcom(user, tree, inp);
+            return Response.status(Response.Status.OK).entity(result).build();
+        }
+        catch (IOException ex)
+        {
+            return Response.status(Response.Status.NOT_ACCEPTABLE).entity(ex.getMessage()).build();
+        }
+        catch (GedcomParserException ex)
+        {
+            return Response.status(Response.Status.NOT_ACCEPTABLE).entity(ex.getMessage()).build();
+        }
 
     }
 
